e4f9bdee997518917b0190546eba298ec1f14463
luluisfcm <1221432@isep.ipp.pt>
2024-11-17 23:08:35 +0000

    update

--- app/src/main/java/com/example/dssmv_projectdroid_1221432_1231479/model/CreateLibraryBookRequest.java
+++ app/src/main/java/com/example/dssmv_projectdroid_1221432_1231479/model/CreateLibraryBookRequest.java
@@ -1,19 +1,16 @@
 package com.example.dssmv_projectdroid_1221432_1231479.model;
 
 public class CreateLibraryBookRequest {
-    private int stock;  // Total number of copies in stock
+    private int stock;
 
-    // Constructor
     public CreateLibraryBookRequest(int stock) {
         this.stock = stock;
     }
 
-    // Getter
     public int getStock() {
         return stock;
     }
 
-    // Optional: You can also add a setter if you need to modify this field
     public void setStock(int stock) {
         this.stock = stock;
     }
